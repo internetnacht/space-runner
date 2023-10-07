58463d6ce99ae30e4da38e07b8cd1b570eb2905c
dexter <philipp.gurtner@stamm-silberfuechse.de>
2023-10-07 22:16:48 +0200

    moving exports to individual constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,11 @@
 import World from "./scenes/World.ts";
 
-const windowWidth = 800
-const windowHeight = 600
+export const windowWidth = 800
+export const windowHeight = 600
 
 const worldIds: number[] = []
 for (let i = 1; i <= 6; i++) {
     worldIds.push(i)
 }
 
-const worlds = worldIds.map(id => new World(`World${id}`, `map${id}`))
-
-export {
-    windowWidth,
-    windowHeight,
-    worlds
-}
+export const worlds = worldIds.map(id => new World(`World${id}`, `map${id}`))
