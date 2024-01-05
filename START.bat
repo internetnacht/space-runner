e8bce1dc7ed2486e20a3002c9d76e1a3be2899d5
pgurtner <81441242+pgurtner@users.noreply.github.com>
2024-01-05 12:25:03 +0100

    build(windows): add automatic closing of already running window

--- START.bat
+++ START.bat
@@ -1,1 +1,18 @@
-npm run dev
+@echo off
+setlocal enabledelayedexpansion
+
+set "customProcessName=start_inacht24_space_runner_server.bat"
+
+REM Check if another instance is already running
+tasklist /fi "windowtitle eq %customProcessName%" 2>nul | find /i "%customProcessName%" >nul
+if %errorlevel% equ 0 (
+    echo Another instance of %customProcessName% is already running. Closing it.
+
+    REM Terminate existing instances
+    taskkill /f /fi "windowtitle eq %customProcessName%"
+    timeout /t 5 >nul
+)
+
+REM Start the server program with the custom process name
+echo Starting %customProcessName%...
+start "" "%customProcessName%"
