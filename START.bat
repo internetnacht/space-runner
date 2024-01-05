6b4803bcd9e59757c23b1219b9f8f484576fc2b7
pgurtner <81441242+pgurtner@users.noreply.github.com>
2024-01-05 12:35:09 +0100

    fix(windows): change start script to bat compatible version

--- START.bat
+++ START.bat
@@ -1,18 +1,20 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "customProcessName=start_inacht24_space_runner_server.bat"
+set "customProcessName=start_inacht24_space_runner_server"
 
 REM Check if another instance is already running
-tasklist /fi "windowtitle eq %customProcessName%" 2>nul | find /i "%customProcessName%" >nul
+wmic process where "CommandLine like '%%customProcessName%%.bat%'" get ProcessId 2>nul | findstr /r /v "^$"
 if %errorlevel% equ 0 (
     echo Another instance of %customProcessName% is already running. Closing it.
 
     REM Terminate existing instances
-    taskkill /f /fi "windowtitle eq %customProcessName%"
+    wmic process where "CommandLine like '%%customProcessName%%.bat%'" call terminate
     timeout /t 5 >nul
 )
 
-REM Start the server program with the custom process name
+REM Start the server batch file with the custom process name
 echo Starting %customProcessName%...
-start "" "%customProcessName%"
+start "" "cmd.exe /c %customProcessName%.bat"
+
+endlocal
