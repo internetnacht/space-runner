fa378734e5702b1e3d4e1e16101b8a2f4eab589a
pgurtner <81441242+pgurtner@users.noreply.github.com>
2024-02-02 13:17:23 +0100

    feat(StartingScreen): add loading text

--- src/scenes/StartingScreen.ts
+++ src/scenes/StartingScreen.ts
@@ -5,8 +5,8 @@ import { GameSettings } from '../components/GameSettings.ts'
 import { loadButtonAssets } from '../components/buttons/button-utils.ts'
 import { FancyClickButton } from '../components/buttons/FancyClickButton.ts'
 import { MovingPlatform } from '../components/map-components/MovingPlatform.ts'
-import { FirebaseTaskUnlocker } from '../auth/FirebaseTaskUnlocker.ts'
 import { LoadingScreen } from './LoadingScreen.ts'
+import { FirebaseTaskUnlocker } from '../auth/FirebaseTaskUnlocker.ts'
 
 export class StartingScreen extends Phaser.Scene {
 	private userSettings: GameSettings
@@ -32,6 +32,24 @@ export class StartingScreen extends Phaser.Scene {
 	}
 
 	create() {
+		const loadingText = this.add.text(
+			MEASURES.window.width / 2,
+			MEASURES.window.height * (2 / 3),
+			'lädt',
+			{
+				backgroundColor: '#000',
+			}
+		)
+		loadingText.x = loadingText.x - loadingText.width / 2
+		loadingText.y = loadingText.y - loadingText.height / 2
+		let dotAmount = 0
+
+		const loop = window.setInterval(() => {
+			dotAmount++
+			dotAmount %= 10
+			loadingText.setText('lädt' + '.'.repeat(dotAmount))
+		}, 1000)
+
 		levels
 			.filter((level) => this.scene.manager.keys[level.id] === undefined)
 			.forEach((level) => this.scene.add(level.id, level))
@@ -81,14 +99,20 @@ export class StartingScreen extends Phaser.Scene {
 		})
 		fancy.center()
 
+		const removeLoading = () => {
+			loadingText.destroy()
+			window.clearInterval(loop)
+		}
+
 		if (FirebaseTaskUnlocker.isSetup()) {
+			removeLoading()
 			fancy.display()
 		} else {
 			FirebaseTaskUnlocker.setup().then(() => {
+				removeLoading()
 				fancy.display()
 			})
 		}
-		//fancy.display()
 
 		this.events.on('shutdown', () => {
 			musicplayer.shutdown()
